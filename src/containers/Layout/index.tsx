b16402ea2c52acc29d7ce66eaae93d058c1bbb12
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-06-08 14:17:24 +0330

    ✨ feat(font): Added Vazir font

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import Header from '@/containers/Layout/Header'
-import Footer from '@/containers/Layout/Footer'
+import React from 'react';
+import Header from '@/containers/Layout/Header';
+import Footer from '@/containers/Layout/Footer';
 
-
-const Layout = ({children}:{children: React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='min-h-screen flex flex-col'>
-        <Header />
-       <div className='flex-1'>
-       {children}
-       </div>
-        <Footer />
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex-1">{children}</div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
